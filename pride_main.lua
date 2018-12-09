57f286e9cdc7105bc68ac0ba7a204b4007b13233
Kathryn Newbould <kathrynrnewbould@gmail.com>
2018-12-09 13:00:36 +0100

    Initial Commit

---
+++ pride_main.lua
@@ -0,0 +1,33 @@
+function desaturate_completed_achievements()
+	--- extend through overwrite the AchievementButton_DisplayAchievement function
+	local originalDisplayAchievement = AchievementButton_DisplayAchievement
+	function AchievementButton_DisplayAchievement(button, category, achievement, selectionID, renderOffScreen)
+		originalDisplayAchievement(button, category, achievement, selectionID, renderOffScreen)
+		local id, name, points, completed, month, day, year, description, flags, icon, rewardText, isGuild, wasEarnedByMe, earnedBy = GetAchievementInfo(category, achievement);
+		if ( completed and not wasEarnedByMe ) then
+			--- if an achievement was not earned by this char, hide the date completed and show the shield as grey
+			button.dateCompleted:Hide();
+			button:Desaturate();
+		end
+		return id
+	end
+	
+			--- find achievement when searching for Incomplete (TEST)
+	local originalGetAchievementInfo = GetAchievementInfo
+	function GetAchievementInfo_TEST(achievementID)
+		local id, name, points, completed, month, day, year, description, flags, icon, rewardText, isGuild, wasEarnedByMe, earnedBy = originalGetAchievementInfo(achievementID)
+		if ( completed and not wasEarnedByMe ) then
+			print(name)
+		end
+		return id, name, points, completed, month, day, year, description, flags, icon, rewardText, isGuild, wasEarnedByMe, earnedBy
+	end
+end
+
+--- only replace the function when the achievement frame is opened (before that it doesn't exist)
+local AchievementDisplayFrame = CreateFrame("Frame")
+AchievementDisplayFrame:RegisterEvent("ADDON_LOADED")
+AchievementDisplayFrame:SetScript("OnEvent", function (self, event, arg)
+	if arg == "Blizzard_AchievementUI" then
+		desaturate_completed_achievements()	 
+	end
+end)
